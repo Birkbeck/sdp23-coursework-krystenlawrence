a3beaf8f313ee94fd85abc6c34f5e68280d2d959
krystenlawrence <94800151+krystenlawrence@users.noreply.github.com>
2023-02-17 19:37:58 +0000

    C2: Add divide, multiply and subtract instructions

--- src/sml/instruction/DivideInstruction.java
+++ src/sml/instruction/DivideInstruction.java
@@ -5,18 +5,18 @@ import sml.Machine;
 import sml.RegisterName;
 
 /**
- * An instruction subclass that takes the values of two registers - r1 and r2 - adds them together and stores the
- * result in the first register - r1.
+ * An instruction subclass that takes the contents of two registers - r1 and r2 - divides the contents of r1 by the
+ * contents of r2 and stores the result in r1.
  * @author
  */
 
-public class AddInstruction extends Instruction {
+public class DivideInstruction extends Instruction {
 	private final RegisterName result;
 	private final RegisterName source;
 
-	public static final String OP_CODE = "add";
+	public static final String OP_CODE = "div";
 
-	public AddInstruction(String label, RegisterName result, RegisterName source) {
+	public DivideInstruction(String label, RegisterName result, RegisterName source) {
 		super(label, OP_CODE);
 		this.result = result;
 		this.source = source;
@@ -26,7 +26,7 @@ public class AddInstruction extends Instruction {
 	public int execute(Machine m) {
 		int value1 = m.getRegisters().get(result);
 		int value2 = m.getRegisters().get(source);
-		m.getRegisters().set(result, value1 + value2);
+		m.getRegisters().set(result, value1 / value2);
 		return NORMAL_PROGRAM_COUNTER_UPDATE;
 	}
 
