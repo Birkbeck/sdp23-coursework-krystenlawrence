9310fdc3524c17a8d8eb2fd714ba810ff30f7e76
krystenlawrence <94800151+krystenlawrence@users.noreply.github.com>
2023-02-18 19:08:02 +0000

    C5: Add print and store instructions tests

---
+++ test/sml/instruction/PrintInstructionTest.java
@@ -0,0 +1,47 @@
+package sml.instruction;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import sml.Instruction;
+import sml.Machine;
+import sml.Registers;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static sml.Registers.Register.EAX;
+import static sml.Registers.Register.EBX;
+
+class PrintInstructionTest {
+
+  private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+  private final PrintStream originalOut = System.out;
+  private Machine machine;
+  private Registers registers;
+
+
+  @BeforeEach
+  void setUp() {
+    machine = new Machine(new Registers());
+    registers = machine.getRegisters();
+    System.setOut(new PrintStream(out));
+  }
+
+  @AfterEach
+  void tearDown() {
+    machine = null;
+    registers = null;
+    System.setOut(originalOut);
+  }
+
+  @Test
+  void executeValid() {
+    registers.set(EAX, 10);
+    Instruction instruction = new PrintInstruction(null, EAX);
+    instruction.execute(machine);
+    Assertions.assertEquals("10\r\n", out.toString());
+  }
+
+}
---
+++ test/sml/instruction/StoreInstructionTest.java
@@ -0,0 +1,45 @@
+package sml.instruction;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import sml.Instruction;
+import sml.Machine;
+import sml.Registers;
+
+import static sml.Registers.Register.EAX;
+import static sml.Registers.Register.EBX;
+
+class StoreInstructionTest {
+  private Machine machine;
+  private Registers registers;
+
+  @BeforeEach
+  void setUp() {
+    machine = new Machine(new Registers());
+    registers = machine.getRegisters();
+  }
+
+  @AfterEach
+  void tearDown() {
+    machine = null;
+    registers = null;
+  }
+
+  @Test
+  void executeValid() {
+    Instruction instruction = new StoreInstruction(null, EAX, 7);
+    instruction.execute(machine);
+    Assertions.assertEquals(7, machine.getRegisters().get(EAX));
+  }
+
+  @Test
+  void executeValidTwo() {
+    registers.set(EAX, -5);
+    registers.set(EBX, 6);
+    Instruction instruction = new AddInstruction(null, EAX, EBX);
+    instruction.execute(machine);
+    Assertions.assertEquals(1, machine.getRegisters().get(EAX));
+  }
+}
